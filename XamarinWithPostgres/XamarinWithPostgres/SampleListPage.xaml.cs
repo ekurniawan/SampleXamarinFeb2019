590a316b737581c3581092cd5484cb91206e2db9
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-02-07 13:44:00 +0700

    menambahkan binding

---
+++ XamarinWithPostgres/XamarinWithPostgres/SampleListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinWithPostgres.ViewModel;
+
+namespace XamarinWithPostgres
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SampleListPage : ContentPage
+    {
+        public SampleListPage()
+        {
+            InitializeComponent();
+            BindingContext = new ListDataViewModel();
+        }
+    }
+}
---
+++ XamarinWithPostgres/XamarinWithPostgres/ViewModel/ListDataViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinWithPostgres.ViewModel
+{
+    public class ListDataViewModel : BindableObject
+    {
+        public ListDataViewModel()
+        {
+            listItems = new List<ListItem> {
+                new ListItem
+                {
+                    Title = "Sate Klathak Pak Jeje",
+                    Description = "Sate Klathak Pak Jeje Pasar Jejeran"
+                },
+                new ListItem {
+                    Title = "Soto Kadipiro",
+                    Description="Soto Kadipiro jalan Wates"
+                },
+                new ListItem{
+                    Title="Bakmi Jawa Mbah Hadi",
+                    Description="Bakmi Jawa Mbah Hadi terban"
+                }
+            };
+        }
+
+        private List<ListItem> listItems;
+        public List<ListItem> ListItems
+        {
+            get { return listItems; }
+            set { listItems = value; OnPropertyChanged("ListItems"); }
+        }
+
+    }
+}
