d93f4f47669d2e2e76c6e71b71fe9c6c0a97fec1
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-02-07 17:08:49 +0700

    menambahkan backend

--- XamarinWithPostgres/XamarinWithPostgres/DaftarRestaurant.xaml.cs
+++ XamarinWithPostgres/XamarinWithPostgres/DaftarRestaurant.xaml.cs
@@ -23,7 +23,12 @@ namespace XamarinWithPostgres
         protected async override void OnAppearing()
         {
             base.OnAppearing();
+            myIndicator.IsRunning = true;
+
             listRestaurant.ItemsSource = await restoService.GetAllData();
+
+            myIndicator.IsRunning = false;
+            myIndicator.IsVisible = false;
         }
     }
 }
